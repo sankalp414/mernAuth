f540c93c997445f456c16515ee5493b489887e1d
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-08-27 21:58:38 +0530

    frontend of the auth

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className='text-9xl bg-green-500 text-pink-500'>Sankalp Sharma</h1>
+    </>
+  )
+}
+
+export default App
